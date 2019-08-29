ff164e9830a17b2c8a8f1aefe912c9d12dd6b62d
Prem <prem.rajah@gmail.com>
2019-08-29 23:16:32 +0100

    Added a validation form Added validation checks

--- src/app/valid-form/valid-form.component.ts
+++ src/app/valid-form/valid-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-valid-form',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./valid-form.component.css']
 })
 export class ValidFormComponent implements OnInit {
+  validationForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.validationForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'
+          ) /* Regex: Atleast one number and letter */
+        ]
+      ],
+      age: [
+        '',
+        [
+          Validators.required,
+          /*String*/ Validators.minLength(2),
+          /*Numerials*/ Validators.min(18),
+          Validators.max(65)
+        ]
+      ],
+      agree: [false, [Validators.requiredTrue]]
+    });
+  }
+
+  get email() {
+    return this.validationForm.get('email');
+  }
+
+  get password(){
+    return this.validationForm.get('password');
+  }
+
+  get age(){
+    return this.validationForm.get('age');
+  }
+
+  get agree(){
+    return this.validationForm.get('agree');
   }
 
 }
